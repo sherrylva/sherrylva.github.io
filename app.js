c31fcf8a7a812e1e3e5fcdcf7a738d5a42235c5e
sherrylva <sherrylva@gmail.com>
2023-07-24 18:20:38 -0700

    added js file

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-const data = await d3.csv("https://sherrylva.github.io/colleges.csv")
+const data = await d3.csv("https://github.com/sherrylva/sherrylva.github.io/blob/main/colleges.csv")
 console.log(data)
